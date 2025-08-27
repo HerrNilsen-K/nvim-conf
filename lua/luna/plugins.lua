3394b86119211d79850ba98a44d15318da70ec98
John git <Nils.Salger@jenoptik.com>
2025-08-27 11:05:10 +0200

    Added which-nvim

--- lua/luna/plugins.lua
+++ lua/luna/plugins.lua
@@ -23,6 +23,26 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"
 	},
 
+	-- Which-key
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+
 	--Neo-tree
 	{
 		{
